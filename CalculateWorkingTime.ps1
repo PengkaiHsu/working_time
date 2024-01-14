0c2ebc6e037529ec0572341ce9c95ecc63f03a92
pengkai.hsu <pengtaikai@hotmail.com>
2024-01-14 16:44:36 +0900

    feature: Code Refactoring, Bug Fix, Comment Modification

--- CalculateWorkingTime.ps1
+++ CalculateWorkingTime.ps1
@@ -11,46 +11,48 @@ $file_data = Get-Content -Encoding UTF8 C:\Programming\PowerShell\WorkingTime\wo
 # 総時間
 $totalTime = 0
 
-# チャージ項目
-$Works = [System.Collections.Generic.Dictionary[String, PSObject]]::new()
+# チャージ項目（チャージ項目名、時間）
+$Works = [System.Collections.Generic.Dictionary[String, Int]]::new()
 
-# チャージ項目詳細
+# チャージ項目詳細（チャージ項目名、チャージ項目詳細）
 $WorksDetail = [System.Collections.Generic.Dictionary[String, String]]::new()
 
 foreach ($row in $file_data)
 {
-  if ($row.StartsWith("## ")){    
-    ## 日付の行の処理 ##
+  if ($row.StartsWith("## ")){
 
-    $date = ($row.Split("## ") | select -First 1 -Last 1)[1]
-    Write-Host $date
-    Write-Host ""
-    
-    $totalTime = 0  
+    ## 日付の行の処理 ##
+    # How does powershell split consecutive strings. Not a single letter: https://stackoverflow.com/questions/76241804/how-does-powershell-split-consecutive-strings-not-a-single-letter
+    $date = ($row.Split([string[]] "## ", 'None'))[1]
+
+    # 日付をプリント
+    Write-Host `n$date`n
+
+    $totalTime = 0
 
   } elseif ($row.StartsWith("- ") -And !($row.Contains("休憩"))){    
-    ## 工数の行の処理 ##
     
+    ## 工数の行の処理 ##
     # この行の工数計算
     $timeSlot = $row.Substring(2, 11) # 時間帯を取得
-    $strStart = ($timeSlot.Split(" ~ ") | select -First 1 -Last 1)[0]
-    $strEnd = ($timeSlot.Split(" ~ ") | select -First 1 -Last 1)[1]
-    $dateTimeStart = [DateTime]::ParseExact($strStart,"HHmm", $null)
-    $dateTimeEnd = [DateTime]::ParseExact($strEnd,"HHmm", $null)
+    $timeStart = ($timeSlot.Split([string[]] " ~ ", 'None'))[0]
+    $timeEnd = ($timeSlot.Split([string[]] " ~ ", 'None'))[1]
+    $dateTimeStart = [DateTime]::ParseExact($timeStart,"HHmm", $null)
+    $dateTimeEnd = [DateTime]::ParseExact($timeEnd,"HHmm", $null)
     
     $timeLength = ($dateTimeEnd - $dateTimeStart)
     $totalTime = $totalTime + $timeLength.TotalMinutes
 
     # この行のチャージ項目判断（ex. LogieTool - 開発定例）
-    $work = (($row.Split(":") | select -First 1 -Last 1)[1]).trim()
+    $work = (($row.Split([string[]] ": ", 'None'))[1]).trim()
 
     if ($work.Contains("-")){
 
         # チャージ項目名（ex. LogieTool）
-        $workName = (($work.Split(" - ") | select -First 1 -Last 1)[0])
+        $workName = ($work.Split([string[]] " - ", 'None'))[0]
 
         # チャージ項目詳細（ex. 開発定例）
-        $workDetail = (($work.Split("-") | select -First 1 -Last 1)[1]).trim()
+        $workDetail = (($work.Split([string[]] " - ", 'None'))[1]).trim()
     }else{
 
         # チャージ項目名（ex. LogieTool）
@@ -68,9 +70,12 @@ foreach ($row in $file_data)
     }
 
     # チャージ項目詳細を重複なしで格納
-    if($WorksDetail.ContainsKey($workDetail)){
+    if($WorksDetail.ContainsKey($workName)){
+        if ($workDetail -ne ""){
+            $WorksDetail[$workName] = $WorksDetail[$workName] + "、" + $workDetail
+        }       
     }else{
-        $WorksDetail.Add($workDetail, $workName)
+        $WorksDetail.Add($workName, $workDetail)
     }
   } 
 }
@@ -82,15 +87,17 @@ foreach($key in $Works.Keys)
     $timePercentage = $value / $totalTime
     $totalPercentage += $timePercentage
     $timePercentage = $timePercentage * 100
+
+    # あるチャージ項目、その総工数、占める割合をプリント
     Write-Host "$key : $value 分 : $timePercentage %"
 }
 
-Write-Host ""
-Write-Host "Total percentage = " $totalPercentage
-Write-Host ""
+Write-Host `n"Total percentage = " $totalPercentage`n
 
 foreach($key in $WorksDetail.Keys)
 {
     $value = $WorksDetail.$key
-    Write-Host "$($value): $key"
+
+    # あるチャージ項目の詳細をプリント（ex. xx project: タスク登録、ブランチ整理）
+    Write-Host "$($key): $value"
 }
