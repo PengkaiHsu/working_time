6835314bdb3531c4c2d4158b88e2f7689967a41f
pengkai.hsu <pengtaikai@hotmail.com>
2024-01-29 17:12:14 +0900

    feature: Code Refactoring (Using Tuple and Group-Object instead of dictionary)

--- CalculateWorkingTime.ps1
+++ CalculateWorkingTime.ps1
@@ -59,37 +59,53 @@ function GetWorkNameAndDetailThisRow([string]$TimeRow) {
 }
 
 # チャージ項目とその工数をプリントするための関数。
-function PrintWorkingTime([System.Collections.Generic.Dictionary[String, Int]]$Works) {
+function PrintWorkingTime([Object[]]$groupedWorksInfo) {
     
-    Write-Host "【① Work title and it's total time】"
+    Write-Host "【1. Work title and it's total time】"
 
+    # あるチャージ項目、その総工数、占める割合を計算する
     $totalPercentage = 0
-    foreach($key in $Works.Keys)
-    {
-        $value = $Works.$key
-        $timePercentage = $value / $totalTime
-        $totalPercentage += $timePercentage
-        $timePercentage = $timePercentage * 100
+    $groupedWorksInfo | ForEach-Object {
 
+        # あるチャージ項目の総工数を集計する。
+        $time = 0
+        $_.Group | ForEach-Object { 
+            $time += $_.Item2
+        }
+
+        # 全体工数で占める割合を計算する。
+        $timePercentage = $time / $totalTime
+        $timePercentage = $timePercentage * 100
+        $totalPercentage += $timePercentage
+        
         # あるチャージ項目、その総工数、占める割合をプリント
-        Write-Host "$key : $value min : $timePercentage %"
+        Write-Host "$($_.Name) : $time min : $timePercentage %"
     }
 
+    # 全体工数をプリント
     Write-Host `n"Total time = " $totalTime
     Write-Host "[for double check]Total percentage = " $totalPercentage`n
 }
 
 # チャージ項目とその詳細をプリントするための関数。
-function PrintWorkingDetail([System.Collections.Generic.Dictionary[String, String]]$WorksDetail) {
+function PrintWorkingDetail([Object[]]$groupedWorksInfo) {
     
-    Write-Host "【② Work title and it's detail】"
+    Write-Host "【2.Work title and it's detail】"
 
-    foreach($key in $WorksDetail.Keys)
-    {
-        $value = $WorksDetail.$key
-
+    # チャージ項目詳細を重複なしで並べる。
+    # About Contains: https://itsakura.com/powershell-contains   
+    $groupedWorksInfo | ForEach-Object {
+        $workDetail = ""
+        $_.Group | ForEach-Object {        
+            if ($workDetail -ne "" -And !($workDetail.Contains($_.Item3)) -And !($_.Item3.Contains($workDetail))){
+                $workDetail = $workDetail + "、" + $_.Item3
+            } else {
+                $workDetail = $_.Item3
+            }
+        }
+        
         # あるチャージ項目の詳細をプリント（ex. xx project: タスク登録、ブランチ整理）
-        Write-Host "$($key): $value"
+        Write-Host "$($_.Name) : $workDetail"
     }
 }
 
@@ -100,10 +116,8 @@ function PrintWorkingDetail([System.Collections.Generic.Dictionary[String, Strin
 # file input
 $file_data = Get-Content -Encoding UTF8 C:\Programming\PowerShell\WorkingTime\working_time\input_sample.md
 
-# チャージ項目の配列（チャージ項目名 + 時間、例えば A project : 60 分）
-$Works = [System.Collections.Generic.Dictionary[String, Int]]::new()
-# チャージ項目詳細の配列（チャージ項目名 + チャージ項目詳細、例えば A project: 開発定例）
-$WorksDetail = [System.Collections.Generic.Dictionary[String, String]]::new()
+# 作業の情報を格納するリスト（チャージ項目名、時間、詳細説明。例えば A project, 60 min, 開発定例）
+$WorksInfo = @()
 # 総工数
 $totalTime = 0
 
@@ -111,45 +125,31 @@ foreach ($row in $file_data)
 {
   if ($row.StartsWith("## ")){
 
-    ## 日付の行の処理：日付をプリント ##
+    ## １．日付の行の処理：日付をプリント ##
     PrintDate -DateRow $row
 
   } elseif ($row.StartsWith("- ") -And !($row.Contains("休憩"))){    
     
-    ## 工数の行の処理：この行の工数を計算 ##
-    $timeLength = GetTimeLengthThisRow -TimeRow $row
-    
+    ## ２．工数の行の処理：この行の工数を計算 ##
+    $timeLength = GetTimeLengthThisRow -TimeRow $row   
     # この行の工数を総工数に足す。
     $totalTime = $totalTime + $timeLength.TotalMinutes
 
-    # この行のチャージ項目名と詳細をGet（ex. A project - 開発定例）
+    # この行のチャージ項目名と詳細をGet（ex. A project, 開発定例）
     $workNameAndDetail = GetWorkNameAndDetailThisRow -TimeRow $row
-    $workName = $workNameAndDetail[0]
-    $workDetail = $workNameAndDetail[1]
-    
-    # チャージ項目名ごとの総工数を計算してチャージ項目の配列へ格納
-    if($Works.ContainsKey($workName)){
-        $Works[$workName] += $timeLength.TotalMinutes
-    }else{
-        $Works.Add($workName, $timeLength.TotalMinutes)
-    }
 
-    # チャージ項目詳細を重複なしでチャージ項目詳細の配列へ格納
-    # About Contains: https://itsakura.com/powershell-contains
-    if($WorksDetail.ContainsKey($workName)){
-        if ($workDetail -ne "" -And !($WorksDetail[$workName].Contains($workDetail)) -And !($workDetail.Contains($WorksDetail[$workName]))){
-            $WorksDetail[$workName] = $WorksDetail[$workName] + "、" + $workDetail
-        }       
-    }else{
-        $WorksDetail.Add($workName, $workDetail)
-    }
+    # この行のチャージ項目名、時間、詳細説明をタプルとしてリストへ格納。
+    $WorksInfo += [Tuple]::Create($workNameAndDetail[0], $timeLength.TotalMinutes, $workNameAndDetail[1])
   } 
 }
 
-# ① チャージ項目（とその工数）をプリントする。
-# ex. B project : 300 分 : 62.5 %
-PrintWorkingTime -Works $Works
+# チャージ項目名、時間、詳細説明のリストを「チャージ項目名」でグループ分けする。
+$groupedWorksInfo = $WorksInfo | Group-Object -Property {$_.Item1}
 
-# ② チャージ項目とその詳細をプリントする。
-# ex. B project: 画面実装
-PrintWorkingDetail -WorksDetail $WorksDetail
+# ① チャージ項目（とその総工数）をプリントする。
+# ex. B project : 300 分 : 62.5 %
+PrintWorkingTime -groupedWorksInfo $groupedWorksInfo
+
+# ② チャージ項目とその詳細説明をプリントする。
+# ex. B project: 画面実装、画面設計
+PrintWorkingDetail -groupedWorksInfo $groupedWorksInfo
